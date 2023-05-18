a1da23215b48a280b24e100b0f36845e7f209de0
Stanislav Vodolagin <ximtech@inbox.lv>
2023-05-19 01:34:13 +0300

    String overrun fix

--- FileUtils.c
+++ FileUtils.c
@@ -426,7 +426,7 @@ static void listFilesInDir(File *directory, fileVector *vec, bool recursive, boo
         file.pathLength = directory->pathLength;
         file.pathLength = concatEndSeparator(file.path, file.pathLength);
 
-        uint32_t dirNameLength = strnlen(inDirectory->d_name, PATH_MAX);
+        uint32_t dirNameLength = strnlen(inDirectory->d_name, FILENAME_MAX);
         strncat(file.path, inDirectory->d_name, dirNameLength);
         file.pathLength += dirNameLength;
         if ((includeDirs || isFile(&file)) && !fileVecAdd(vec, file)) {
