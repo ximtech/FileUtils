a2ab418aa4450b9bcce68450cce5b26c5aaad566
Stanislav Vodolagin <ximtech@inbox.lv>
2023-05-23 12:30:30 +0300

    Remove compile

--- FileUtils.c
+++ FileUtils.c
@@ -423,7 +423,7 @@ static void listFilesInDir(File *directory, fileVector *vec, bool recursive, boo
         file.pathLength = concatEndSeparator(file.path, file.pathLength);
 
         uint32_t dirNameLength = strlen(inDirectory->d_name);
-        strncat(file.path, inDirectory->d_name, dirNameLength);
+        strcat(file.path, inDirectory->d_name);
         file.pathLength += dirNameLength;
         if ((includeDirs || isFile(&file)) && !fileVecAdd(vec, file)) {
             closedir(directory->dir);
